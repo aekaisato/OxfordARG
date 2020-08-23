b828a9aa5566f26d76f200082185dab6b514ad0b
realToadtoad <kingdomkeepersguy@gmail.com>
2020-08-23 12:43:48 -0700

    fixed shadow overlap

--- oxford-arg-ui/app/layouts/phase1_layout/phase1_window.tsx
+++ oxford-arg-ui/app/layouts/phase1_layout/phase1_window.tsx
@@ -45,8 +45,6 @@ export class Phase1Window extends React.Component<WindowProps> {
               borderBottomWidth: 4,
               borderLeftWidth: 4,
               borderRightWidth: 4,
-              shadowOpacity: 0.35,
-              shadowRadius: 20,
             },
           ]}
         >
