5c45f2b8e5e0a3ebc0387530a31197c939352a7b
Aiden Sato <kingdomkeepersguy@gmail.com>
2021-02-04 19:12:44 -0800

    added instructions for flow

--- oxford-arg-ui/app/puzzles/puzzle_1_robotfix1/puzzle_1.tsx
+++ oxford-arg-ui/app/puzzles/puzzle_1_robotfix1/puzzle_1.tsx
@@ -4,23 +4,73 @@ import {
   View,
   Dimensions,
   ImageBackground,
+  Image,
+  Text,
+  Button,
 } from "react-native";
 
-// import {goto, increment} from "../../components/status_system/status_system"
+let deviceWidth = Dimensions.get("window").width;
 
 import html from "./flow-free-master/flow.html";
 
-
 export class Puzzle1 extends React.Component {
+  state: { infoOverlay: "flex" | "none" } = {
+    infoOverlay: "flex",
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <ImageBackground
-          style={[styles.container, {alignItems: "center"}]}
+          style={[styles.container, { alignItems: "center" }]}
           source={require("../../../assets/backdrops/puzzle_1.jpg")}
         >
-          <div dangerouslySetInnerHTML={{ __html: html }} style={{boxShadow: "0px 0px 20px"}}></div>
+          <div
+            dangerouslySetInnerHTML={{ __html: html }}
+            style={{ boxShadow: "0px 0px 20px" }}
+          ></div>
         </ImageBackground>
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#000000A0",
+            display: this.state.infoOverlay,
+          }}
+        >
+          <Image
+            source={require("../../../assets/img/flow-instructions.webp")}
+            style={{
+              width: "88.8vh",
+              height: "50vh",
+              borderColor: "white",
+              borderWidth: 5,
+            }}
+            resizeMode="contain"
+          />
+          <View style={{ height: 25 }} />
+          <View style={{ flexDirection: "row" }}>
+            <Text
+              style={{
+                fontFamily: "Noto-Sans",
+                fontSize: deviceWidth / 64,
+                color: "white",
+              }}
+            >
+              Click and drag.
+            </Text>
+            <View style={{ width: 25 }} />
+            <View>
+              <Button
+                title="Understood."
+                onPress={() => this.setState({ infoOverlay: "none" })}
+              />
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
