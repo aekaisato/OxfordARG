f1088fc8913559211ff47d896b935ba79b2a3475
Aiden Sato <kingdomkeepersguy@gmail.com>
2021-03-10 16:53:09 -0800

    made icon fix

--- oxford-arg-ui/build.sh
+++ oxford-arg-ui/build.sh
@@ -6,8 +6,8 @@ curl -O https://dl.nwjs.io/v0.52.0/nwjs-v0.52.0-win-x64.zip
 curl -O https://dl.nwjs.io/v0.52.0/nwjs-v0.52.0-linux-x64.tar.gz
 curl -O https://dl.nwjs.io/v0.52.0/nwjs-v0.52.0-osx-x64.zip
 cp nwjs-pkg.json web-build/package.json
-cd web-build
 cp assets/icon.png web-build/
+cd web-build
 zip -r package.nw *
 cd ..
 mkdir -p nw/linux nw/mac nw/win
